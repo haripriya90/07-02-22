b4fcc4202137ea4ccc5db559a8447122f485a870
Lenovo <Lenovo@SBSH>
2022-02-07 12:08:40 +0530

    comit

---
+++ javaVariables/src/javaVariables/ex1.java
@@ -0,0 +1,44 @@
+package javaVariables;
+
+public class ex1 {
+
+		public static void main(String[] args)
+		{
+			//variable declaration
+			
+			int studentId;
+			String studentName;
+			int studentAge;
+			float studentMarks;
+			char studentGrade;
+			boolean studentResult;
+			String studentAddress;
+			
+			//variable intialization
+			
+			studentId=1005;
+			studentName="priya";
+			studentAge=21;
+			studentMarks=75.05f;
+			studentGrade='A';
+			studentResult=true;
+			studentAddress="hyderabad";
+			
+			System.out.print("studentId: ");
+			System.out.println(studentId);
+			System.out.print("studentName: ");
+			System.out.println(studentName);
+			System.out.print("studentAge: ");
+			System.out.println(studentAge);
+		    System.out.print("studentMarks: ");
+			System.out.println(studentMarks);
+			System.out.print("studentGrade: ");
+			System.out.println(studentGrade);
+			System.out.print("studentResult: ");
+			System.out.println(studentResult);
+            System.out.print("studentAddress: ");
+			System.out.println(studentAddress);
+		}
+	}
+
+	
